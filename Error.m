39846b66eff6de8d5133f47f11cea1730fd09b31
vansh2552 <87716952+vansh2552@users.noreply.github.com>
2023-06-26 19:31:00 +0530

    Create Error.m

---
+++ Error.m
@@ -0,0 +1,9 @@
+function Error()
+    bs=padv.BuildService.get(pwd);
+    var=bs.getLastProcessModelResults;
+    for idx=1:length(var)
+        if contains(var(idx).TaskKey, "Actuator_Control") && var(idx).TaskState == 'Executed'
+            error('Execution halted.');
+        end
+    end    
+end
